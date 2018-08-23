8e0d227c0fc41bff107bd522e101e74f27f7cd59
ccarmonr <carlos.m.carmona.r@gmail.com>
2018-08-22 22:29:05 -0500

    sincronizacion desde eclipse

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Carlos que nota, %s!", someone);
+    return String.format("Carlos que nota dos, %s!", someone);
   }
 }
