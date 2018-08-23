49b672c390ece7a0cdef43b6894fc1de90490151
ccarmonr <carlos.m.carmona.r@gmail.com>
2018-08-22 22:20:10 -0500

    sdfsd

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("rio, %s!", someone);
+    return String.format("dos, %s!", someone);
   }
 }
